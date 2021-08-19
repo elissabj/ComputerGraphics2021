302fa1278f1f2866737f5a177db81a8c1b27cdec
elissabj <56558029+elissabj@users.noreply.github.com>
2021-08-19 14:11:41 -0500

    corrections and comments for practice 1

--- Practice1/practice1.c
+++ Practice1/practice1.c
@@ -1,44 +1,68 @@
+/*
+INSTITUTO POLITÉCNICO NACIONAL 
+Escuela Superior de Cómputo
+Computer Graphics 
+Ramos Gómez Elisa
+Practice 1
+3CV14
+
+        Recordando que la formula para la pendiente es
+                Y2 - Y1
+            m = -------
+                X2 - X1 
+
+        Dependiendo de que parte sea la mayor, el barrido 
+        de los puntos es creciente y calcularemos el valor 
+        faltante (x o y) segun sea el caso.
+ 
+*/
+
 #include<stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main(void){
 
-    int x1,y1, x2, y2;
+    int x1,y1, x2, y2;                                          //Declaramos los puntos
 
-    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);                   //Leemos los puntos 
 
-    printf("%d, %d, %d, %d\n", x1, y1, x2, y2);
 
-    int cota_infX, cota_supX;
-  	int cota_infY, cota_supY;
-  
-  	if(x1 > x2){
-        cota_infX = x2;
-        cota_supX = x1;
-    }else{
-        cota_infX = x1;
-        cota_supX = x2;
-    }
-  	
-  	if(y1 > y2){
-    	cota_infY = y2;
-        cota_supY = y1;
-    }else{
-        cota_infY = y1;
-        cota_supY = y2;
-    }
+    int pendiente = (y2 - y1) / (x2 - x1);                      //Declaracion y calculo de la pendiente
+    int i, j;                                                   //Declaro iteradores
 
-    //printf("%d, %d : %d, %d\n", cota_infX, cota_supX, cota_infY, cota_supY);
+    printf("\nX\tY\n");                                         //Imprimimos el encabezado de la tabla
 
-    printf("X\tY\n");
-    int i, j;
-    for(i = cota_infX; i <= cota_supX; i++){
-        for(j = cota_infY; j <= cota_supY; j++){
-            int x = i*(y2-y1); int y = j * (x2+x1);
-            int c = -(y2*x1) + (y1*x2);
-            printf("%d\t%d\n", i, j);
+    if(abs(y2-y1) < abs(x2-x1)){                                //Si x gano en valor 
+
+        int cota_infX, cota_supX;                               //Calculo cotas
+
+        if(x1 > x2){
+            cota_infX = x2; cota_supX = x1;
+        }else{
+            cota_infX = x1; cota_supX = x2;
         }
-    } 
 
+        for(i = cota_infX; i <= cota_supX; i++){
+            j = (y1*(x2-x1) + i * (y2-y1) - x1*(y2-y1)) / (x2-x1);      //calculo del Y por cada punto X
+            printf("%d\t%d\n", i, j);  
+        } 
+        
+
+    }else{
+
+        int cota_infY, cota_supY;
+        if(y1 > y2){
+    	    cota_infY = y2; cota_supY = y1;
+        }else{
+            cota_infY = y1; cota_supY = y2;
+        }
+        
+        for(i = cota_infY; i <= cota_supY; i++){            
+            j = (x1*(y2-y1) + i *(y2-y1) - y1*(y2-y1)) / (y2-y1);        //calculo del X por cada punto Y
+            printf("%d\t%d\n", j, i);            
+        }
+
+    }
 
 return 0;
 }
