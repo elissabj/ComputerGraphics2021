2f12b80f98689be8cb7100c4970136ebb1cae80b
elissabj <56558029+elissabj@users.noreply.github.com>
2021-09-20 14:03:04 -0500

    Add files via upload

---
+++ Practice 2/RenderAlgorithm.h
@@ -0,0 +1,17 @@
+int min (int a, int b);                                                     //Function to obtain MIN value for int 
+
+int max (int a, int b);                                                     //Function to obtain MAX value for int
+
+void swapPoints (int x1, int y1, int x2, int y2);                           //Function to make swap for a pair of points
+
+void NaiveApproach (int x1, int y1, int x2, int y2);                        //Algorithm 1
+
+void Bresenham(int x1, int y1, int x2, int y2);                             //Algorithm 2
+
+void DDA (int x1, int y1, int x2, int y2);                                  //Algorithm 3
+
+void goesToMagic(void);                                                     //Function to draw in the .ppm File
+
+
+
+
