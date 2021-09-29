a8b5a1fc91eadbc6e960553ab529f7f171042ace
elissabj <56558029+elissabj@users.noreply.github.com>
2021-09-28 20:10:41 -0500

    Update RenderAlgorithm.h

--- Practice 2/RenderAlgorithm.h
+++ Practice 2/RenderAlgorithm.h
@@ -2,8 +2,6 @@ int min (int a, int b);                                                     //Fu
 
 int max (int a, int b);                                                     //Function to obtain MAX value for int
 
-void swapPoints (int x1, int y1, int x2, int y2);                           //Function to make swap for a pair of points
-
 void NaiveApproach (int x1, int y1, int x2, int y2);                        //Algorithm 1
 
 void Bresenham(int x1, int y1, int x2, int y2);                             //Algorithm 2
