cb4ce2c5b146958f61e094a8af3954c8e45d29e1
elissabj <56558029+elissabj@users.noreply.github.com>
2021-10-17 23:32:16 -0500

    Update DrawLinePPM.c

--- Practice 2/DrawLinePPM.c
+++ Practice 2/DrawLinePPM.c
@@ -4,20 +4,19 @@
 #include "DrawLinePPM.h"
 #include "RenderAlgorithm.h"
 
-void drawFigure (RGB matrixToDraw[MAXN][MAXN]){                                                                             //To draw in a .ppm File
+void drawFigure (RGB matrixToDraw[MAXN][MAXN]){                                                                                  //To draw in a .ppm File
 
     int i, j;
 
-    FILE  *fobj = fopen("model.ppm", "wb");                                                                                 //Open the .ppm File and write the new result
-    (void) fprintf(fobj, "P3\n%d %d\n255\n", MAXN, MAXN);
+    FILE  *fobj = fopen("model.ppm", "wb");
+    (void) fprintf(fobj, "P3\n%d %d\n255\n", MAXN, MAXN);                                                                         //Open the .ppm File and write the new result
 
-    for(i = 0; i  < MAXN; i++){
+    for(i = MAXN-1; i  >= 0; i--){
         for(j = 0; j < MAXN; j++){
 
-            fprintf(fobj, "%d %d %d\n", matrixToDraw[i][j].red, matrixToDraw[i][j].green, matrixToDraw[i][j].blue);
-            
+            fprintf(fobj, "%d %d %d\n", matrixToDraw[j][i].red, matrixToDraw[j][i].green, matrixToDraw[j][i].blue);          
         }
     }
-
-return;
+    
+    return;
 }
