7fe513c64a25e7608e5a50a785c3a6df0ee2c0fd
elissabj <56558029+elissabj@users.noreply.github.com>
2021-10-19 03:01:29 -0500

    Add files via upload

---
+++ Practice 3/Point.h
@@ -0,0 +1,21 @@
+#define MAXSIZE 4                                                                           //Static size for faces per line, this can modify 
+
+typedef struct Point{                                                                       //Data Type Point 
+    
+    float x;
+    float y;
+    float z; 
+    float w;   
+}Point;
+
+typedef struct Faces{                                                                       //Data Type Faces
+
+    int connections[MAXSIZE];
+
+}Faces;
+
+float minPoint ( float a, float b);                                                         //Function to calculate the MIN for floats
+float maxPoint ( float a, float b);                                                         //Function to calculate the MAX for floats
+
+void coordinatesPositives(Point *points, float miniPointX, float miniPointY, int i);        //Move the points to the fisrt octant
+void printFaces(Faces * arr_face, int MAXI, Point *points, int ctFrame);                    //Make the magic with the RederAlgorithm library
